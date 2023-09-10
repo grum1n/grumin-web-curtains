cbed566fed3dbc68f299f4ec0649618899cf32c1
grum1n <grumin.el.pastas@gmail.com>
2023-09-10 11:14:52 +0300

    changed text to lithuanian

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,9 @@
             <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)">
                 <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
                     <div>
-                        <h2 class="text-4xl font-bold text-white">Curtains</h2>
+                        <h2 class="text-4xl font-bold text-white">{{ __('Curtains') }}</h2>
 
-                        <p class="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit</p>
+                        <p class="max-w-xl mt-3 text-gray-300">{{ __('Geriausias užuolaidų salonas Kaune') }}</p>
                     </div>
                 </div>
             </div>
@@ -18,7 +18,7 @@
                         <a href="{{ route('home') }}">
                             <x-frontend.app-logo />
                         </a>
-                        <p class="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+                        <p class="mt-3 text-gray-500 dark:text-gray-300">{{ __('Vartotojo paskyra') }}</p>
                     </div>
 
                     <x-validation-errors class="mb-4" />
@@ -33,12 +33,12 @@
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
                             <div>
-                                <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+                                <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">{{ __('Email') }}</label>
                                 <input 
                                     type="email" 
                                     name="email" 
                                     id="email" 
-                                    placeholder="example@example.com" 
+                                    placeholder="pavyzdys@pavyzdys.com" 
                                     :value="old('email')" 
                                     required
                                     autofocus 
@@ -49,15 +49,15 @@
 
                             <div class="mt-6">
                                 <div class="flex justify-between mb-2">
-                                    <label for="password" class="text-sm text-gray-600 dark:text-gray-200">Password</label>
-                                    <a href="#" class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</a>
+                                    <label for="password" class="text-sm text-gray-600 dark:text-gray-200">{{ __('Slaptažodis') }}</label>
+                                    <a href="#" class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">{{ __('Užmiršote slaptažodį?') }}</a>
                                 </div>
 
                                 <input 
                                     type="password" 
                                     name="password" 
                                     id="password" 
-                                    placeholder="Your Password" 
+                                    placeholder="Jūsų slaptažodis" 
                                     required
                                     autocomplete="current-password" 
                                     class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
@@ -66,19 +66,19 @@
                             <div class="block mt-4">
                                 <label for="remember_me" class="flex items-center">
                                     <x-checkbox id="remember_me" name="remember" />
-                                    <span class="ml-2 text-sm text-gray-400">{{ __('Remember me') }}</span>
+                                    <span class="ml-2 text-sm text-gray-400">{{ __('Prisiminti mane') }}</span>
                                 </label>
                             </div>
 
                             <div class="mt-6">
                                 <button class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                                    Sign in
+                                    {{ __('Prisijungti') }}
                                 </button>
                             </div>
 
                         </form>
 
-                        <p class="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="{{ route('register') }}" class="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p>
+                        <p class="mt-6 text-sm text-center text-gray-400">Dar neturite paskyros? <a href="{{ route('register') }}" class="text-blue-500 focus:outline-none focus:underline hover:underline">{{ __('Registruotis') }}</a>.</p>
                     </div>
                 </div>
             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,9 +5,9 @@
             <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)">
                 <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
                     <div>
-                        <h2 class="text-4xl font-bold text-white">Curtains</h2>
+                        <h2 class="text-4xl font-bold text-white">{{ __('Curtains') }}</h2>
 
-                        <p class="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit</p>
+                        <p class="max-w-xl mt-3 text-gray-300">{{ __('Geriausias užuolaidų salonas Kaune') }}</p>
                     </div>
                 </div>
             </div>
@@ -19,7 +19,7 @@
                         <a href="{{ route('home') }}">
                             <x-frontend.app-logo />
                         </a>
-                        <p class="mt-3 text-gray-500 dark:text-gray-300">New user register form</p>
+                        <p class="mt-3 text-gray-500 dark:text-gray-300">{{ __('Sukurti naują paskyrą') }}</p>
                     </div>
 
                     <x-validation-errors class="mb-4" />
@@ -35,23 +35,23 @@
                             @csrf
 
                             <div>
-                                <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Name</label>
-                                <input type="text" name="name" id="name" placeholder="Enter Name" :value="old('name')" required autofocus autocomplete="name" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">{{ __('Vardas') }}</label>
+                                <input type="text" name="name" id="name" placeholder="Vardas" :value="old('name')" required autofocus autocomplete="name" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
 
                             <div class="mt-6">
-                                <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
-                                <input type="email" name="email" id="email" placeholder="example@example.com" :value="old('email')" required autocomplete="username" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">{{ __('Email') }}</label>
+                                <input type="email" name="email" id="email" placeholder="pavyzdys@pavyzdys.com" :value="old('email')" required autocomplete="username" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
 
                             <div class="mt-6">
-                                <label for="password" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Password</label>
-                                <input type="password" name="password" id="password" placeholder="Your Password" required autocomplete="new-password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                <label for="password" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">{{ __('Slaptažodis') }}</label>
+                                <input type="password" name="password" id="password" placeholder="Jūsų slaptažodis" required autocomplete="new-password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
 
                             <div class="mt-6">
-                                <label for="password_confirmation" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Confirm Password</label>
-                                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                <label for="password_confirmation" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">{{ __('Patvirtinkite slaptažodį') }}</label>
+                                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Patvirtinkite savo slaptažodį" required autocomplete="new-password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
 
                             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
@@ -73,13 +73,13 @@
 
                             <div class="mt-6">
                                 <button class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                                    Register
+                                    {{ __('Register') }}
                                 </button>
                             </div>
 
                         </form>
 
-                        <p class="mt-6 text-sm text-center text-gray-400">Already registered? <a href="{{ route('login') }}" class="text-blue-500 focus:outline-none focus:underline hover:underline">Sign in</a>.</p>
+                        <p class="mt-6 text-sm text-center text-gray-400">Jau turite paskyrą? <a href="{{ route('login') }}" class="text-blue-500 focus:outline-none focus:underline hover:underline">{{ __('Prisijungti') }}</a>.</p>
                     </div>
                 </div>
             </div>
