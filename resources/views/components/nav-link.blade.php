29c3225d9ee6608d73dc50a1ac77a383d98a116f
grum1n <grumin.el.pastas@gmail.com>
2023-09-13 16:18:26 +0300

    changed text navbar color

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'inline-flex items-center px-1 pt-1 border-b-2 border-lime-400 text-lg font-medium leading-5 text-lime-300 focus:outline-none focus:border-lime-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-lg font-medium leading-5 text-lime-100 hover:text-gray-300 hover:border-gray-300 focus:outline-none focus:text-lime-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-lg font-medium leading-5 text-lime-50 hover:text-gray-300 hover:border-gray-300 focus:outline-none focus:text-lime-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
